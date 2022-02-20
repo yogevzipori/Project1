85c08e194b5f834814d11ed1e9d8409fe5b650f5
Yogev Zipori <yogevzipori@Yogevs-MacBook-Pro.local>
2022-02-20 11:59:30 -0800

    nextlevel

--- code.js
+++ code.js
@@ -1,40 +1,40 @@
-let player1turn = true
+// Players
+let player1turn = true;
 
+// Marking on the board x and o
+const gameBody = document.querySelector(".game-body");
+gameBody.addEventListener("click", function (e) {
+  if (player1turn === true && e.target.innerText != "O"&& e.target.innerText != "X") {
+    e.target.innerText = "X";
+    player1turn = false;
+  } else if (player1turn === false && e.target.innerText != "O" && e.target.innerText != "X") {
+    e.target.innerText = "O";
+    player1turn = true;
+  } checkWinningCondition()
+});
 
-const player1 = document.querySelector('.game-body')
-player1.addEventListener('click', function(e){
-    if( player1turn === true){
-        e.target.innerText = 'X';
-    player1turn = false
-
-    }
-
-else{ 
-    e.target.innerText = 'O';
-player1turn = true
-
-}
-})
-
-
+// Winning possibilities
 
 const winCombos = [
-[0, 1, 2, 3],
-[4, 5, 6, 7],
-[8, 9, 10, 11],
-[12, 13, 14, 15],
-[12, 9, 6, 3],
-[0, 5, 10, 15],
-[3, 6, 9, 12],
-[3, 7, 11, 15],
-[2,6, 10, 14],
-[1, 5, 9, 13 ],
-[0, 4, 8, 12]
+  [0, 1, 2, 3],
+  [4, 5, 6, 7],
+  [8, 9, 10, 11],
+  [12, 13, 14, 15],
+  [12, 9, 6, 3],
+  [0, 5, 10, 15],
+  [3, 6, 9, 12],
+  [3, 7, 11, 15],
+  [2, 6, 10, 14],
+  [1, 5, 9, 13],
+  [0, 4, 8, 12]
 ];
 
+function checkWinningCondition(){
+
+}
 
 
-document.querySelector('.gameReset').addEventListener('click', function(){
-location.reload();
-  
-  });
+// Resetting Game Board
+document.querySelector(".gameReset").addEventListener("click", function () {
+  location.reload();
+});
